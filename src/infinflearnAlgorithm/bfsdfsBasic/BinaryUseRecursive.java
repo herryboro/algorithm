e54585af0b87cf63280f58d94b8b20935b8f1754
herryboro <herryboro@naver.com>
2023-02-15 15:46:45 +0900

    - 10진수 -> 2진수 변환  재귀함수 풀이

--- src/infinflearnAlgorithm/bfsdfsBasic/BinaryUseRecursive.java
+++ src/infinflearnAlgorithm/bfsdfsBasic/BinaryUseRecursive.java
@@ -4,19 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Recursive {
-    public void recursive(int n) {
-        if(n == 0) {
+public class BinaryUseRecursive {
+    public void binary(int n) {
+        if(n == 1) {
+            System.out.print(1 + " ");
             return;
         } else {
-            recursive(n-1);
+            binary(n/2);
         }
-        System.out.print(n);
+        System.out.print(n % 2 + " ");
     }
     public static void main(String[] args) throws IOException {
-        Recursive recur = new Recursive();
+        BinaryUseRecursive recursive = new BinaryUseRecursive();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        recur.recursive(n);
+        recursive.binary(n);
     }
 }
