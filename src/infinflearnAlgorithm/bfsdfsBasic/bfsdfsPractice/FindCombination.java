8fc0302ef9e3ad2ff5aeaf341d341ec455fb39ca
herryboro <herryboro@naver.com>
2023-03-01 23:00:07 +0900

    - 조합 구하기

---
+++ src/infinflearnAlgorithm/bfsdfsBasic/bfsdfsPractice/FindCombination.java
@@ -0,0 +1,33 @@
+package infinflearnAlgorithm.bfsdfsBasic.bfsdfsPractice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FindCombination {
+    static int[] answer;
+    static int n, m;
+    public void solution(int level, int s) {
+        if(level == m) {
+            for (int num : answer) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = s; i <= n; i++) {
+                answer[level] = i;
+                solution(level + 1, i + 1);
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        FindCombination fc = new FindCombination();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        n = Integer.parseInt(s[0]);
+        m = Integer.parseInt(s[1]);
+
+        answer = new int[m];
+        fc.solution(0, 1);
+    }
+}
---
+++ src/infinflearnAlgorithm/bfsdfsBasic/bfsdfsPractice/GuessProgression.java
@@ -0,0 +1,16 @@
+package infinflearnAlgorithm.bfsdfsBasic.bfsdfsPractice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GuessProgression {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int f = Integer.parseInt(s[1]);
+
+
+    }
+}
