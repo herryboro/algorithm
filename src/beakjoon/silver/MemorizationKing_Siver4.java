c7aba585c1879808deb1cb7fdcde79c89df75bc2
herryboro <herryboro@naver.com>
2023-03-11 18:36:22 +0900

    - 백준 암기왕  이진탐색 Arrays. binarySearch(arr, num) 메소드 기능 사용없이 구현

--- src/beakjoon/silver/MemorizationKing_Siver4.java
+++ src/beakjoon/silver/MemorizationKing_Siver4.java
@@ -1,11 +1,39 @@
 package beakjoon.silver;
 
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class MemorizationKing_Siver4 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int t = Integer.parseInt(br.readLine());
+//
+//        for(int a=0; a<t; a++) {
+//            Set<Integer> set = new HashSet<>();
+//            StringBuilder sb = new StringBuilder();
+//            int n = Integer.parseInt(br.readLine());
+//
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//            for(int i=0; i<n; i++)
+//                set.add(Integer.parseInt(st.nextToken()));
+//
+//            int m = Integer.parseInt(br.readLine());
+//            st = new StringTokenizer(br.readLine());
+//
+//            for(int i=0; i<m; i++) {
+//                if (set.contains(Integer.parseInt(st.nextToken()))) {
+//                    sb.append(1).append("\n");
+//                } else {
+//                    sb.append(0).append("\n");
+//                }
+//            }
+//            System.out.println(sb);
+//        }
+//    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
@@ -14,13 +42,13 @@ public class MemorizationKing_Siver4 {
         for (int i = 0; i < t; i++) {
             // note1 필기장
             int n = Integer.parseInt(br.readLine());
-            ArrayList<Integer> list = new ArrayList<>();
             int[] arr = new int[n];
             String[] line = br.readLine().split(" ");
 
             for (int j = 0; j < n; j++) {
                 arr[j] = Integer.parseInt(line[j]);
             }
+
             Arrays.sort(arr);
 
             // note2 필기장
@@ -29,15 +57,31 @@ public class MemorizationKing_Siver4 {
 
             for (int j = 0; j < m; j++) {
                 int num = Integer.parseInt(line[j]);
-                int index = Arrays.binarySearch(arr, num);
+                boolean found = false;
+                int left = 0;
+                int right = arr.length - 1;
 
-                if (index >= 0) {
+                while (left <= right) {
+                    int mid = (left + right) / 2;
+
+                    if (arr[mid] == num) {
+                        found = true;
+                        break;
+                    } else if (arr[mid] < num) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+
+                if (found) {
                     sb.append("1\n");
                 } else {
                     sb.append("0\n");
                 }
             }
         }
+
         System.out.print(sb.toString());
     }
 }
