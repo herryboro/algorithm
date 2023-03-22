d2b793d3f76bd4aad27fcec12432b1ed84538ce5
herryboro <herryboro@naver.com>
2023-03-22 22:01:12 +0900

    - 프로그래머스 다리를 지나는 트럭

--- src/programmers/level2/stackqueue/TruckPassingBridge.java
+++ src/programmers/level2/stackqueue/TruckPassingBridge.java
@@ -7,30 +7,24 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class TruckPassingBridge {
-
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
         Queue<Integer> queue = new LinkedList<>();
-
-        // bridge_length 길이 만큼 queue 초기값 0으로 세팅
         for (int i = 0; i < bridge_length; i++) {
             queue.offer(0);
         }
 
         int index = 0;
-        // 다리 위의 총 차량 무게
-        int weightSum = 0;
+        int sumWeight = 0;
 
-        while(index < truck_weights.length) {
-            // 다리에서 나갈 차량 poll
-            int outCar = queue.poll();
-            // 나간 차량의 무게를 다리 위에 있는 차량의 총 무게에서 뺌
-            weightSum -= outCar;
+        while (index < truck_weights.length) {
+            int wcfb = queue.poll(); // mean: the weight of the car in front of the bridge
 
-            // 총 차량 무게와 한도 초과 무게를 비교시 작거나 같은 경우에는 다음 차량이 다리 위로 들어와도 됨
-            if(weightSum + truck_weights[index] <= weight) {
+            sumWeight -= wcfb;
+
+            if (sumWeight + truck_weights[index] <= weight) {
                 queue.offer(truck_weights[index]);
-                weightSum += truck_weights[index];
+                sumWeight += truck_weights[index];
                 index++;
             } else {
                 queue.offer(0);
@@ -40,6 +34,7 @@ public class TruckPassingBridge {
 
         return answer + bridge_length;
     }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
