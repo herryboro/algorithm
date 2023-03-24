a5b52f2f82e85642949dc21382ad324bc85ba53a
herryboro <herryboro@naver.com>
2023-03-24 17:01:44 +0900

    - git ssh 변경 확인용

--- src/programmers/level1/PaintOver.java
+++ src/programmers/level1/PaintOver.java
@@ -1,8 +1,11 @@
 package programmers.level1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 // 프로그래머스 1단계 덧칠하기
 public class PaintOver {
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
