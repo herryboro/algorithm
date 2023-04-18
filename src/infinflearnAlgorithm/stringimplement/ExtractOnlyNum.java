09f1c41a98013ae2768fd47292669fa0d97efaaa
herryboro <herryboro@naver.com>
2023-04-18 23:36:15 +0900

    -문자열 구현 가장 짧은 거리

--- src/infinflearnAlgorithm/stringimplement/ExtractOnlyNum.java
+++ src/infinflearnAlgorithm/stringimplement/ExtractOnlyNum.java
@@ -8,6 +8,6 @@ public class ExtractOnlyNum {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-
+        System.out.println(s);
     }
 }
